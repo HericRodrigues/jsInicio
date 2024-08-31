da27003faff0ca9f5b49c8d51154edee0d55833c
Heric Rodrigues Silva <83992975+HericRodrigues@users.noreply.github.com>
2024-08-31 13:38:51 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+alert('Bem-vindo ao joguinho');
+let numeroSecreto = 4;
+console.log('numeroSecreto')
+let chute;
+let tent = 1;
+
+
+//cometarios
+while(chute != numeroSecreto){
+
+    chute = prompt('Escolha um número de 1 a 30');
+    if (chute == numeroSecreto) {
+       break;
+    } else {
+        if (chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            if (chute < numeroSecreto) {
+                alert(`O número secreto é maior que ${chute}`);
+            }
+            tent++;
+        }
+    }
+}   
+
+if (tent > 1) {
+    alert(`Você descobriu o número secreto ${numeroSecreto} com ${tent} tentativas.`);
+} else {
+    alert(`Você descobriu o número secreto ${numeroSecreto} com ${tent} tentativa.`);
+}
+
+
+/*Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+let contador = 1;
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
+}
+Crie um contador que comece em 10 e vá até 0 usando um loop while. Mostre cada número.
+let contador = 10;
+while (contador >= 0) {
+    console.log(contador);
+    contador--;
+}
+Crie um programa de contagem regressiva. Peça um número ao usuario e conte deste número até 0, usando um loop while no console do navegador.
+let numeroMaximo = prompt("Digite um número para a contagem regressiva:");
+
+while (numeroMaximo >= 0) {
+    console.log(numeroMaximo);
+    numeroMaximo--;
+}
+Crie um programa de contagem progressiva. Peça um número ao usuario e conte de 0 até esté número, usando um loop while no console do navegador.
+let numeroMaximo = prompt("Digite um número para a contagem progregressiva:");;
+let contador = 0;
+
+while (contador <= numeroMaximo) {
+    console.log(contador);
+    contador++
+}
+*/
+
